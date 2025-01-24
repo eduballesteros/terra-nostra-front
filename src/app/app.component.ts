9bd3891280eaa62708d1d2fe22e60ea5272e4fd3
DESKTOP-P2VQDL1\csi22 <ballesterospereze@gmail.com>
2025-01-24 11:57:28 +0100

    Add Info and Search Box components; update Navbar and Carousel styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { CarouselComponent } from "./components/carousel/carousel.component";
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { InfoComponent } from './components/info/info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, CarouselComponent],
+  imports: [RouterOutlet, NavbarComponent, CarouselComponent, SearchBoxComponent, InfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
