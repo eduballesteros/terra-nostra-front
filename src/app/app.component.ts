6352b017f461db7087a8460b33c0382af9edfa31
DESKTOP-P2VQDL1\csi22 <ballesterospereze@gmail.com>
2025-01-29 09:27:05 +0100

    Refactor landing page components; remove unused components and add new module structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./components/navbar/navbar.component";
-import { CarouselComponent } from "./components/carousel/carousel.component";
-import { SearchBoxComponent } from './components/search-box/search-box.component';
-import { InfoComponent } from './components/info/info.component';
+import { NavbarComponent } from "./features/landing-page/components/navbar/navbar.component"; 
+import { CarouselComponent } from "./features/landing-page/components/carousel/carousel.component";
+import { SearchBoxComponent } from './features/landing-page/components/search-box/search-box.component';
+import { InfoComponent } from './features/landing-page/components/info/info.component';
 
 @Component({
   selector: 'app-root',
